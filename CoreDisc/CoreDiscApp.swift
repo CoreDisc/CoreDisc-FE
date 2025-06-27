be273c3bf405665528b6b4a6f9a1e20748866dfd
alwn8918 <alwn8918@gmail.com>
2025-06-28 02:36:48 +0900

    Initial Commit

---
+++ CoreDisc/CoreDiscApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDiscApp.swift
+//  CoreDisc
+//
+//  Created by 김미주 on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDiscApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
