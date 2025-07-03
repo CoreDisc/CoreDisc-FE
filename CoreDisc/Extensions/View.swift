38823f65982bde23172712d71e56de91792cb852
alwn8918 <alwn8918@gmail.com>
2025-07-04 03:13:46 +0900

    Feat: 선형 그라데이션 modifier 구현

---
+++ CoreDisc/Extensions/View.swift
@@ -0,0 +1,33 @@
+//
+//  View.swift
+//  CoreDisc
+//
+//  Created by 김미주 on 7/4/25.
+//
+
+import SwiftUI
+
+// MARK: - 그라데이션
+
+// Linear
+struct LinearGradientModifier: ViewModifier {
+    var startColor: Color
+    var endColor: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                LinearGradient(
+                    gradient: Gradient(colors: [startColor, endColor]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+            )
+    }
+}
+
+extension View {
+    func linearGradient(startColor: Color, endColor: Color) -> some View {
+        modifier(LinearGradientModifier(startColor: startColor, endColor: endColor))
+    }
+}
