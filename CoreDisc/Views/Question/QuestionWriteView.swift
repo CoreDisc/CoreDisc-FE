a8024d57b477fb868c6b76cd28580970840255b1
codmsdlrltgjqm <chaeni1218@naver.com>
2025-07-13 02:40:22 +0900

    ✨ [Feat] 작성 질문 요약 페이지 UI 구현

--- CoreDisc/Views/Question/QuestionWriteView.swift
+++ CoreDisc/Views/Question/QuestionWriteView.swift
@@ -236,39 +236,6 @@ struct QuestionWriteView: View {
     
 }
 
-// 카테고리 선택 컴포넌트
-struct CategorySelectButton: View {
-    var title: String
-    var width: CGFloat
-    
-    @Binding var selectedCategory: String?
-    
-    var isSelected: Bool {
-        selectedCategory == title
-    }
-    
-    var body: some View {
-        Button(action: {
-            if isSelected {
-                selectedCategory = nil
-            } else {
-                selectedCategory = title
-            }
-        }) {
-            ZStack(alignment: .leading) {
-                Rectangle()
-                    .frame(width: width, height: 60)
-                    .foregroundStyle(isSelected ? Color("key") : .black)
-                Text("\(title)")
-                    .textStyle(.Q_Main)
-                    .foregroundStyle(isSelected ? .black: .white)
-                    .padding(.leading, 19)
-            }
-            
-        }
-    }
-}
-
 
 
 #Preview {
