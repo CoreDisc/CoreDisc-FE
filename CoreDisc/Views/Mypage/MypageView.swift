2f26480d39649e987a255052cb978f54971eb765
alwn8918 <alwn8918@gmail.com>
2025-07-13 07:02:30 +0900

    ✨ 마이페이지 메인 레이아웃 1차 구현 (#20)

--- CoreDisc/Views/Mypage/MypageView.swift
+++ CoreDisc/Views/Mypage/MypageView.swift
@@ -9,11 +9,125 @@ import SwiftUI
 
 struct MypageView: View {
     var body: some View {
-        VStack {
-            Text("Pretendard")
-                .textStyle(.Title_Text_Ko)
-            Text("Preform")
-                .textStyle(.Title_Text_Eng)
+        ZStack {
+            Image(.imgShortBackground2)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer().frame(height: 3)
+                
+                TopMenuGroup
+                
+                ProfileGroup
+                
+                Spacer().frame(height: 13)
+                
+                CountGroup
+                
+                Spacer().frame(height: 25)
+                
+                Button(action: {}) { // TODO: action
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(.key)
+                            .frame(height: 39)
+                            .padding(.horizontal, 24)
+                        
+                        Text("Edit Profile")
+                            .textStyle(.Pick_Q_Eng)
+                            .foregroundStyle(.black000)
+                    }
+                }
+                .buttonStyle(.plain)
+                
+                Spacer()
+            }
+        }
+    }
+    
+    // 상단 메뉴
+    private var TopMenuGroup: some View {
+        HStack(spacing: 10) {
+            RoundedRectangle(cornerRadius: 12)
+                .fill(.highlight)
+                .frame(width: 28, height: 55)
+            
+            Spacer()
+            
+            Button(action: {}) { // TODO: action
+                Image(.iconCalendar)
+                    .foregroundStyle(.white)
+            }
+            
+            Button(action: {}) { // TODO: action
+                Image(.iconSetting)
+                    .foregroundStyle(.white)
+            }
+        }
+        .offset(x: -14)
+    }
+    
+    // 프로필 영역
+    private var ProfileGroup: some View {
+        VStack(spacing: 8) {
+            Circle() // TODO: 프로필 이미지
+                .frame(width: 124, height: 124)
+            
+            Text("@music_sama")
+                .textStyle(.Pick_Q_Eng)
+                .foregroundStyle(.gray100)
+            
+            Text("닉네임")
+                .textStyle(.Button_s)
+                .foregroundStyle(.gray100)
+        }
+    }
+    
+    // disc, followers, following
+    private var CountGroup: some View {
+        HStack(spacing: 0) {
+            // disc
+            VStack {
+                Text("153") // TODO: 게시글 수 반영
+                    .textStyle(.Q_Main)
+                    .foregroundStyle(.white)
+                
+                Text("disc")
+                    .textStyle(.Q_Sub)
+                    .foregroundStyle(.gray400)
+            }
+            .frame(width: 100, height: 40)
+            
+            // follower
+            Button(action: {}) { // TODO: action
+                VStack {
+                    Text("522")
+                        .textStyle(.Q_Main)
+                        .foregroundStyle(.white)
+                    
+                    Text("followers")
+                        .textStyle(.Q_Sub)
+                        .foregroundStyle(.gray400)
+                }
+            }
+            .frame(width: 100, height: 40)
+            .buttonStyle(.plain)
+            
+            // following
+            Button(action: {}) { // TODO: action
+                VStack {
+                    Text("921")
+                        .textStyle(.Q_Main)
+                        .foregroundStyle(.white)
+                    
+                    Text("following")
+                        .textStyle(.Q_Sub)
+                        .foregroundStyle(.gray400)
+                }
+            }
+            .frame(width: 100, height: 40)
+            .buttonStyle(.plain)
         }
     }
 }
