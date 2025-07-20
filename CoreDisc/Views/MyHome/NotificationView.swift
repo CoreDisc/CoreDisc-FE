3f2fcd70bc4cb0db0e49dc05f13778801925d021
alwn8918 <alwn8918@gmail.com>
2025-07-21 02:46:10 +0900

    ✨ 알림 설정 - 알림 시간 설정 구현 (#34)

--- CoreDisc/Views/MyHome/NotificationView.swift
+++ CoreDisc/Views/MyHome/NotificationView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct NotificationView: View {
+    @StateObject var notificationViewModel: NotificationViewModel = .init()
     @Environment(\.dismiss) var dismiss
     
     // 토글 상태
     @State private var firstToggleOn: Bool = false
     @State private var secondToggleOn: Bool = false
+    
+    // 시간
+    @State var showSheet: Bool = false
 
     var body: some View {
         ZStack {
@@ -25,11 +29,25 @@ struct NotificationView: View {
                 
                 TopMenuGroup
                 
+                Spacer().frame(height: 13)
+                
                 ToggleGroup
+                
+                Spacer().frame(height: 11.6)
+                
+                TimeGroup
                 
                 Spacer()
             }
+            
+            if showSheet {
+                TimePickerSheet(showSheet: $showSheet, timeType: notificationViewModel.timeType)
+                    .environmentObject(notificationViewModel)
+                    .transition(.move(edge: .bottom))
+                    .zIndex(1)
+            }
         }
+        .animation(.easeInOut(duration: 0.3), value: showSheet)
         .toolbarVisibility(.hidden, for: .navigationBar)
     }
     
@@ -84,7 +102,34 @@ struct NotificationView: View {
         .padding(.horizontal, 38)
     }
     
-    // 질문 알림 수신
+    // 질문 알림 시간
+    private var TimeGroup: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("질문 알림 시간")
+                .textStyle(.A_Main)
+                .foregroundStyle(.white)
+                .padding(.leading, 6)
+            
+            Spacer().frame(height: 8)
+            
+            NotificationTimeBox(
+                title: "첫 번째 재알림 시간",
+                timeType: .first,
+                showSheet: $showSheet,
+            )
+            .environmentObject(notificationViewModel)
+            
+            Spacer().frame(height: 11.6)
+            
+            NotificationTimeBox(
+                title: "두 번째 재알림 시간",
+                timeType: .second,
+                showSheet: $showSheet
+            )
+            .environmentObject(notificationViewModel)
+        }
+        .padding(.horizontal, 38)
+    }
 }
 
 // MARK: - components
@@ -147,8 +192,57 @@ struct NotificationToggle: View {
     }
 }
 
-// 알림 시간
+// 알림 시간 박스
+struct NotificationTimeBox: View {
+    @EnvironmentObject var notificationViewModel: NotificationViewModel
+    
+    var title: String
+    var timeType: TimeType
+    @Binding var showSheet: Bool
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .fill(.gray600)
+                .frame(height: 54.3)
+                
+            HStack {
+                Text(title)
+                    .textStyle(.Button_s)
+                    .foregroundStyle(.gray200)
+                    .padding(.leading, 6)
+                
+                Spacer()
+                
+                // 시간
+                ZStack {
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(.gray400)
+                        .frame(width: 60, height: 24)
+                    
+                    Button(action: {
+                        notificationViewModel.timeType = timeType
+                        showSheet = true
+                    }) {
+                        switch timeType {
+                        case .first:
+                            Text("\(String(format: "%02d", notificationViewModel.firstHour)) : \(String(format: "%02d", notificationViewModel.firstMinute))")
+                                .textStyle(.Q_Main)
+                                .foregroundStyle(.black000)
+                        case .second:
+                            Text("\(String(format: "%02d", notificationViewModel.secondHour)) : \(String(format: "%02d", notificationViewModel.secondMinute))")
+                                .textStyle(.Q_Main)
+                                .foregroundStyle(.black000)
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal, 19)
+        }
+    }
+}
 
 #Preview {
     NotificationView()
+        .environmentObject(NotificationViewModel())
 }
