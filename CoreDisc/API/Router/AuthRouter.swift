59b0b25341712936142757fd7d7ab928b31375d2
alwn8918 <alwn8918@gmail.com>
2025-07-24 21:40:56 +0900

    ✨ AuthRouter 주석 추가 및 순서 수정 (#43)

--- CoreDisc/API/Router/AuthRouter.swift
+++ CoreDisc/API/Router/AuthRouter.swift
@@ -10,20 +10,18 @@ import Moya
 
 // /api/auth API 연결
 enum AuthRouter {
-    case postSignup(signupData: SignupData)
+    case postVerifyCode(verifyCodeData: VerifyCodeData) // 이메일 코드 인증
+    case postUsername(usernameData: UsernameData) // 아이디 찾기
+    case postSignup(signupData: SignupData) // 회원가입
+    case postSendCode(email: String) // 이메일 인증 메일 전송
+    case postReissue // 토큰 재발급
+    case postVerifyUser(verifyUserData: VerifyUserData) // 비밀번호 변경을 위한 사용자 검증
+    case postLogout // 로그아웃
+    case postLogin(loginData: LoginData) // 일반 로그인
     
-    case getCheckEmail(email: String)
-    case getCheckUsername(username: String)
-    case getCheckNickname(nickname: String)
-    
-    case postSendCode(email: String)
-    case postVerifyCode(verifyCodeData: VerifyCodeData)
-    case postLogin(loginData: LoginData)
-    // 소셜 로그인
-    case postReissue
-    case postLogout
-    case postUsername(usernameData: UsernameData)
-    case postVerifyUser(verifyUserData: VerifyUserData)
+    case getCheckUsername(username: String) // 아이디 중복 확인
+    case getCheckNickname(nickname: String) // 닉네임 중복 확인
+    case getCheckEmail(email: String) // 이메일 중복 확인
 }
 
 extension AuthRouter: APITargetType {
@@ -31,68 +29,66 @@ extension AuthRouter: APITargetType {
     
     var path: String {
         switch self {
+        case .postVerifyCode:
+            return "\(Self.authPath)/verify-code"
+        case .postUsername:
+            return "\(Self.authPath)/username"
         case .postSignup:
             return "\(Self.authPath)/signup"
+        case .postSendCode:
+            return "\(Self.authPath)/send-code"
+        case .postReissue:
+            return "\(Self.authPath)/reissue"
+        case .postVerifyUser:
+            return "\(Self.authPath)/verify-user"
+        case .postLogout:
+            return "\(Self.authPath)/logout"
+        case .postLogin:
+            return "\(Self.authPath)/login"
             
-        case .getCheckEmail:
-            return "\(Self.authPath)/check-email"
         case .getCheckUsername:
             return "\(Self.authPath)/check-username"
         case .getCheckNickname:
             return "\(Self.authPath)/check-nickname"
-            
-        case .postSendCode:
-            return "\(Self.authPath)/send-code"
-        case .postVerifyCode:
-            return "\(Self.authPath)/verify-code"
-        case .postLogout:
-            return "\(Self.authPath)/logout"
-        case .postReissue:
-            return "\(Self.authPath)/reissue"
-        case .postLogin:
-            return "\(Self.authPath)/login"
-        case .postUsername:
-            return "\(Self.authPath)/username"
-        case .postVerifyUser:
-            return "\(Self.authPath)/verify-user"
+        case .getCheckEmail:
+            return "\(Self.authPath)/check-email"
         }
     }
     
     var method: Moya.Method {
         switch self {
-        case .getCheckEmail, .getCheckUsername, .getCheckNickname:
-            return .get
-        case .postSignup, .postSendCode, .postVerifyCode, .postLogout, .postReissue, .postLogin, .postUsername, .postVerifyUser:
+        case .postVerifyCode, .postUsername, .postSignup, .postSendCode, .postReissue, .postVerifyUser, .postLogout, .postLogin:
             return .post
+        case .getCheckUsername, .getCheckNickname, .getCheckEmail:
+            return .get
         }
     }
     
     var task: Task {
         switch self {
+        case .postVerifyCode(let verifyCodeData):
+            return .requestJSONEncodable(verifyCodeData)
+        case .postUsername(let usernameData):
+            return .requestJSONEncodable(usernameData)
         case .postSignup(let signupData):
             return .requestJSONEncodable(signupData)
+        case .postSendCode(let email):
+            return .requestParameters(parameters: ["email": email], encoding: URLEncoding.default)
+        case .postVerifyUser(let verifyUserData):
+            return .requestJSONEncodable(verifyUserData)
+        case .postReissue:
+            return .requestPlain
+        case .postLogout:
+            return .requestPlain
+        case .postLogin(let loginData):
+            return .requestJSONEncodable(loginData)
             
-        case .getCheckEmail(let email):
-            return .requestParameters(parameters: ["email": email], encoding: URLEncoding.queryString)
         case .getCheckUsername(let username):
             return .requestParameters(parameters: ["username": username], encoding: URLEncoding.queryString)
         case .getCheckNickname(let nickname):
             return .requestParameters(parameters: ["nickname": nickname], encoding: URLEncoding.queryString)
-            
-        case .postSendCode(let email):
-            return .requestParameters(parameters: ["email": email], encoding: URLEncoding.default)
-        case .postVerifyCode(let verifyCodeData):
-            return .requestJSONEncodable(verifyCodeData)
-        case .postLogout:
-            return .requestPlain
-        case .postReissue:
-            return .requestPlain
-        case .postLogin(let loginData):
-            return .requestJSONEncodable(loginData)
-        case .postUsername(let usernameData):
-            return .requestJSONEncodable(usernameData)
-        case .postVerifyUser(let verifyUserData):
-            return .requestJSONEncodable(verifyUserData)
+        case .getCheckEmail(let email):
+            return .requestParameters(parameters: ["email": email], encoding: URLEncoding.queryString)
         }
     }
 }
