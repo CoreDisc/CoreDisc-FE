8723769b8b8b340f444ca044fd8f8545bf5e6d6b
mzxxzysy <jsyjj21@gmail.com>
2025-07-26 16:08:26 +0900

    ✨ 통계 리포트 메인 화면 퍼블리싱 (#30)
    1차 퍼블리싱, 애니메이션 X

---
+++ CoreDisc/Models/Report/ReportFirstQuestionModel.swift
@@ -0,0 +1,15 @@
+//
+//  ReportFirstQuestionModel.swift
+//  CoreDisc
+//
+//  Created by 정서영 on 7/19/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct ReportFirstQuestionModel: Identifiable {
+    let id = UUID()
+    let image: Image
+    let question: String
+}
