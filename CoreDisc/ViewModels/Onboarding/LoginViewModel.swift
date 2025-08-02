e284a51eb500fa41dc008a613aebaa3032ff96ac
mzxxzysy <jsyjj21@gmail.com>
2025-08-02 17:17:12 +0900

    💄 로그인 에러 메세지 띄우기 (#5)

--- CoreDisc/ViewModels/Onboarding/LoginViewModel.swift
+++ CoreDisc/ViewModels/Onboarding/LoginViewModel.swift
@@ -12,6 +12,7 @@ class LoginViewModel: ObservableObject {
     @Published var username: String = ""
     @Published var password: String = ""
     @Published var isLogin = false
+    @Published var isError = false
     
     private let authProvider = APIManager.shared.createProvider(for: AuthRouter.self)
     
@@ -36,15 +37,19 @@ class LoginViewModel: ObservableObject {
                         print("로그인 성공 : \(String(describing: decodedResponse.result))")
                         DispatchQueue.main.async {
                             self.isLogin = true
+                            self.isError = false
                         }
                     } else{
                         print("로그인 실패 : \(decodedResponse.message)")
+                        self.isError = true
                     }
                 } catch {
                     print("로그인 디코딩 오류 : \(error.localizedDescription)")
+                    self.isError = true
                 }
             case .failure(let error):
                 print("로그인 API 오류 : \(error.localizedDescription)")
+                self.isError = true
             }
         }
     }
